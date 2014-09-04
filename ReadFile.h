cd95d13b632bf42d41bd2cb93b2411920bbbe734
Trevor Summers <tssummers42@students.tntech.edu>
2014-09-04 14:03:15 -0500

    your comments for the commit

---
+++ ReadFile.h
@@ -0,0 +1,26 @@
+#if !defined READ_FILE
+#define READ_FILE
+
+#include "Text.h"
+
+#include <fstream>
+using namespace std;
+
+class ReadFile
+{
+   private:
+   ifstream input_file;
+   bool _eof;
+   bool closed;
+
+   public:	
+
+};
+
+ReadFile* createReadFile(const char* file_name);
+void destroyReadFile(ReadFile* rf);
+String* readLine(ReadFile* rf);
+bool eof(ReadFile* rf);
+void close(ReadFile* rf);
+
+#endif
